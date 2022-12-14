43d6c2a9d78e1028afc93b356cc2ee9fd765be9e
Muthurajchella- <muthuraj.chella@gmail.com>
2022-12-14 19:05:52 +0530

    Math

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+//Ceil it is defind us TOP 
+document.write(Math.ceil(1.5));
+
+//fooler it is  defind us down
+document.write(Math.floor(3.6));
+
+//Round method 
+document.write(Math.round(4.8));
+
+//sqrt 
+document.write(Math.sqrt(8,2));
+
+//power method
+document.write(Math.pow(2,5));
+
+//min 
+document.write(Math.min(5,5,8,9,1));
+
+//max
+document.write(Math.max(5,5,8,9,1));
+
+//sin
+document.write(Math.sin(90*Math.PI/180));
+
+//cos
+document.write(Math.cos(90*Math.PI/180));
+
+//random 
+document.write(Math.random());
+
+//abs
+document.write(Math.abs(-4.6));
